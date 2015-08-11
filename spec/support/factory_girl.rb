2fc77f08ca99d9ea15cb16cd99cae3a38ff8bbf0
Joshua Mun <joshmun23@gmail.com>
2015-08-11 16:42:35 -0400

    set up project tests

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,4 +1,5 @@
 require 'factory_girl'
+require 'date'
 
 FactoryGirl.define do
   factory :user do
@@ -7,4 +8,16 @@ FactoryGirl.define do
     password_confirmation 'password'
   end
 
+  factory :project do
+    sequence(:company_name) {|n| "Company #{n}"}
+    sequence(:company_city) {|n| "City #{n}"}
+    sequence(:company_state) {|n| "Location #{n}"}
+    sequence(:start_date) { |n| Time.now - (n+rand(2..60)).months }
+    sequence(:end_date) { |n| Time.now - (n+rand(0..2)).months }
+    title "Full-Stack Web Developer"
+    user
+  end
+
+  factory :admin, class: User do
+  end
 end
